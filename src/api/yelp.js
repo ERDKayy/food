5df8da3820a0e3b9b87c11388bdfc18bc902644e
Eric Kimball <Eric-k93@live.com>
2021-09-26 22:00:23 -0700

    Delete yelp.js

--- src/api/yelp.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.yelp.com/v3/businesses',
-    headers: {
-        Authorization: 'Bearer gqAF1ZcU3RKpiNtvrRpbe8B1suDVSI7v7_uxZVJSN18JtEdbrMooz-cNB1URrTPV5PVMPQhQIxGh2xtjwzEY7RyMrxvdxuZZmMIlS7CIsfHKmG9XOdI2JQ4YJxJRYXYx'
-    }
-});
